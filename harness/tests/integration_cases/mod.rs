813d5bd5a7ec4e14ead5feedbf0bb42364fbb4f7
EinsteinDB <71530574+whtcorpsinc@users.noreply.github.com>
2021-11-04 04:57:58 -0700

    fetching original
    adding state machine replication

--- harness/tests/integration_cases/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 EinsteinDB Project Authors. Licensed under Apache-2.0.
-
-mod test_violetabft;
-mod test_violetabft_flow_control;
-mod test_violetabft_paper;
-mod test_violetabft_snap;
-mod test_raw_node;
